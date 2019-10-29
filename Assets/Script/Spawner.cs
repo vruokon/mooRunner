e0f31ad22e20331b5d5a03a80563be1d21d34de5
Mwirtanen <miikka.wirtanen@edu.turkuamk.fi>
2019-10-29 19:42:14 +0200

    Satunnaista vaihtelua esteiden synnyn ajoitukseen

--- Assets/Script/Spawner.cs
+++ Assets/Script/Spawner.cs
@@ -9,17 +9,26 @@ public GameObject parentObject;
 public GameObject objectRotation;
 
 private float timeBtwSpawn;
-public float startTimeBtwSpawn;
+private float startTimeBtwSpawn;
 public float decreaseTime;
+
+
+ void Start() 
+{ 
+    startTimeBtwSpawn = Random.Range(7, 10);  
+}
 
 
 void Update()
 {
+    
     if (timeBtwSpawn <= 0) {
         var myObject = Instantiate(obstacle, transform.position, Quaternion.identity);
         myObject.transform.parent = parentObject.transform;
         myObject.transform.rotation = objectRotation.transform.rotation; 
         
+        startTimeBtwSpawn = Random.Range(7, 10);
+        Debug.Log(startTimeBtwSpawn);
         timeBtwSpawn = startTimeBtwSpawn;
     }   else {
             timeBtwSpawn -= Time.deltaTime;
