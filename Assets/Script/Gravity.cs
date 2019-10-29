a709f70b4c19a8881eb1d2a9af8de580563984a4
Mwirtanen <miikka.wirtanen@edu.turkuamk.fi>
2019-10-29 14:12:43 +0200

    Kuun painovoima lisätty

---
+++ Assets/Script/Gravity.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    public float massOfMoon;
+    public Transform centerOfMoon;
+    public float G;
+
+    float massOfplayer;
+    float distance;
+    float forceValue;
+    Vector3 forceDirection;
+
+    Rigidbody2D rbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        massOfplayer = rbody.mass;
+        distance = Vector3.Distance(centerOfMoon.position, transform.position);
+        forceValue =  G * (massOfMoon * massOfplayer) / (distance * distance);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        forceDirection = centerOfMoon.position - transform.position;
+        rbody.AddForce(forceValue * forceDirection);
+    }
+}
