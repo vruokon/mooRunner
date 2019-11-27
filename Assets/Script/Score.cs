28fe6078e6d34b89af53dd539e6ca75530177979
Viljami Ruokonen <vruokonen@gmail.com>
2019-11-27 16:50:25 +0200

    Lisätty pisteiden lasku

---
+++ Assets/Script/Score.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Score : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text score;
+    public int scoreAmount;
+    private Text scoreText;
+
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        scoreAmount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "Score: " + scoreAmount;
+        scoreAmount++;
+    }
+}
