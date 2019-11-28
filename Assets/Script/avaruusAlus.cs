e37b86454e0536b695dc10fb6f25a6562a17a4b5
Viljami Ruokonen <vruokonen@gmail.com>
2019-11-28 19:44:07 +0200

    lisäsin avaruusaluksen, ja sain sen liikkumaan. Pitää vielä lisätä pysähdys, ja lehmän tiputus

---
+++ Assets/Script/avaruusAlus.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class avaruusAlus : MonoBehaviour
+{
+    float moveSpeed = 1.1f;
+    private Rigidbody2D alus;
+    // Start is called before the first frame update
+    void Start()
+    {
+        alus = transform.GetComponent<Rigidbody2D>();
+        alus.velocity = new Vector2(-moveSpeed, alus.velocity.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        alus.velocity = new Vector2(-moveSpeed, alus.velocity.y);
+    }
+
+}
