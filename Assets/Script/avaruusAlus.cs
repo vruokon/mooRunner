ad1910f10c5d1471b13f82ed34d9e244db3d0fcd
Mwirtanen <miikka.wirtanen@edu.turkuamk.fi>
2019-11-30 18:22:45 +0200

    Muutin avaruusaluksen liikettä
    + Alus pysähtyy nyt hetkeksi pelaajan ylle ja jatkaa sitten matkaansa.

--- Assets/Script/avaruusAlus.cs
+++ Assets/Script/avaruusAlus.cs
@@ -5,32 +5,67 @@ using UnityEngine;
 public class avaruusAlus : MonoBehaviour
 {
     float moveSpeed = 1.1f;
+
+    private bool isMoving = true;
+    private bool continueMoving = false;
     private Rigidbody2D alus;
     private AudioManager audioManager;
+
+    private Transform target;
+    private Vector3 targetPos;
     
     void Start()
     {
-        alus = transform.GetComponent<Rigidbody2D>();
-        alus.velocity = new Vector2(-moveSpeed, alus.velocity.y);
+        target = GameObject.FindWithTag("Cow").transform;
 
-         //caching
+        //caching
         audioManager = AudioManager.instance;
         if( audioManager == null)
         Debug.LogError("No AudioManager found in scene"); 
 
           audioManager.PlaySound("Ufo");
+        
+        //alus = transform.GetComponent<Rigidbody2D>();
+        //alus.velocity = new Vector2(-moveSpeed, alus.velocity.y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        alus.velocity = new Vector2(-moveSpeed, alus.velocity.y);   
-    }
+        //alus.velocity = new Vector2(-moveSpeed, alus.velocity.y);  
+        //audioManager.PlaySound("Ufo");
+       
+       float step = moveSpeed * Time.deltaTime;
 
+        targetPos = transform.position;
+
+        if (targetPos.x == target.position.x){
+            isMoving = false;
+            StartCoroutine(Wait());                
+        }
+
+        if (isMoving == true){
+            targetPos.x = target.transform.position.x;
+            transform.position = Vector3.MoveTowards (transform.position, targetPos, step);
+        }
+
+        if (continueMoving == true){
+           transform.position += Vector3.left * step;
+        }
+         
+    }
 
     // Tuhoaa objektin, kun se on kadonnut kameran alueelta
     void OnBecameInvisible() {
-         Destroy(gameObject);
+        Destroy(gameObject);
+    }
+
+    // Odottaa pelaajan paikalla hetken aikaa
+    IEnumerator Wait(){
+        yield return new WaitForSeconds(3);
+        continueMoving = true;
+       
+        
     }
 
 }
